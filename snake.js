5cfcdcbaf25667a0f78394dc492f04366164b4df
Marcel Akerboom <makerboom@mborijnland.nl>
2021-09-14 14:25:15 +0200

    Mooi blokje getekend

--- snake.js
+++ snake.js
@@ -1,3 +1,7 @@
 let canvas = document.getElementById('snake');
 canvas.style.border = "1px solid black";
 
+let context = canvas.getContext('2d');
+
+context.fillStyle = 'grey';
+context.fillRect(20, 50, 10, 10);
