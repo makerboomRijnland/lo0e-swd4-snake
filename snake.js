1b9ac49a7408fd0f9f7cfb38a1f7db10dfb56785
Marcel Akerboom <makerboom@mborijnland.nl>
2021-09-14 14:41:31 +0200

    drawSnakePart function

--- snake.js
+++ snake.js
@@ -1,12 +1,24 @@
+const WIDTH = 10;
+const HEIGHT = 10;
+
 let canvas = document.getElementById('snake');
 canvas.style.border = "1px solid black";
 
 let context = canvas.getContext('2d');
 
-context.fillStyle = 'grey';
+function drawSnakePart(x, y) {
+    context.fillStyle = 'grey';
+    context.fillRect(x, y, WIDTH, HEIGHT);
+}
 
-context.fillRect(10, 10, 10, 10);
-context.fillRect(20, 10, 10, 10);
-context.fillRect(30, 10, 10, 10);
-context.fillRect(30, 20, 10, 10);
-context.fillRect(30, 30, 10, 10);
+drawSnakePart(10, 10);
+drawSnakePart(20, 10);
+drawSnakePart(30, 10);
+drawSnakePart(30, 20);
+drawSnakePart(30, 30);
+
+// context.fillRect(10, 10, WIDTH, HEIGHT);
+// context.fillRect(20, 10, WIDTH, HEIGHT);
+// context.fillRect(30, 10, WIDTH, HEIGHT);
+// context.fillRect(30, 20, WIDTH, HEIGHT);
+// context.fillRect(30, 30, WIDTH, HEIGHT);
