3c16f4f4d92ac2bae8b10632e0f92afaf1d2da54
Marcel Akerboom <makerboom@mborijnland.nl>
2021-09-14 14:28:48 +0200

    Snake 5 blokken met bocht.

--- snake.js
+++ snake.js
@@ -4,4 +4,9 @@ canvas.style.border = "1px solid black";
 let context = canvas.getContext('2d');
 
 context.fillStyle = 'grey';
-context.fillRect(20, 50, 10, 10);
+
+context.fillRect(10, 10, 10, 10);
+context.fillRect(20, 10, 10, 10);
+context.fillRect(30, 10, 10, 10);
+context.fillRect(30, 20, 10, 10);
+context.fillRect(30, 30, 10, 10);
